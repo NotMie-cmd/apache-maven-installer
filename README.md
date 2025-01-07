0d6a46e42411ff5c5df6eec8b5aba8ca9e9cab34
Misko <notmiepl@gmail.com>
2025-01-07 14:35:44 +0100

    Change not being to download error without the Maven folder
    coolness

--- README.md
+++
@@ -1,5 +0,0 @@
-# apache-maven-installer
-i made a Apache Maven installer.
-
-## the installer saves Maven (mvn) at C:\Program Files\Maven\Maven{version}-\apache-maven-{version}\
-### Also makes a system path and Path variable so it could work!
--- main.cs
+++ main.cs
@@ -11,13 +11,19 @@ namespace MavenInstaller
     {
         static async Task Main(string[] args)
         {
-            string mavenVersion = "3.9.9"; // Specify the Maven version you want to install
+            string mavenVersion = "3.9.9";
             string installPath = $@"C:\Program Files\Maven\Maven{mavenVersion}";
             string zipFilePath = Path.Combine(Path.GetTempPath(), $"apache-maven-{mavenVersion}-bin.zip");
             string downloadUrl = $"https://downloads.apache.org/maven/maven-3/{mavenVersion}/binaries/apache-maven-{mavenVersion}-bin.zip";
 
             try
             {
+                if (!Directory.Exists(installPath))
+                {
+                    Console.WriteLine($"Creating installation directory at {installPath}...");
+                    Directory.CreateDirectory(installPath);
+                }
+
                 using (HttpClient client = new HttpClient())
                 {
                     Console.WriteLine("Downloading Maven...");
@@ -43,12 +49,18 @@ namespace MavenInstaller
                 }
 
                 Console.WriteLine("Maven installed successfully!");
+                await Task.Delay(1000);
+                Console.WriteLine("Closing...");
+                await Task.Delay(4000);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
             }
-            finally {
+            finally
+            {
                 if (File.Exists(zipFilePath))
                 {
                     File.Delete(zipFilePath);
